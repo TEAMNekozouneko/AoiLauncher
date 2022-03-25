d52af9b0e47af84f4ee594083f7a2125a4918c26
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-25 14:15:19 +0900

    [Update] Vanilla-<Version>に変更

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -711,7 +711,7 @@ async function dlAsync(login = true){
                 const gameStateChange = function(data){
                     data = data.trim()
                     if(SERVER_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('50人クラフトに参加中!')
+                        DiscordWrapper.updateDetails('あるサーバーに参加中!')
                     } else if(GAME_JOINED_REGEX.test(data)){
                         DiscordWrapper.updateDetails('マインクラフトをプレイ中!')
                     }
@@ -772,7 +772,7 @@ async function dlAsync(login = true){
                     // 一定時間経ったらLoading表示を解除
                     setTimeout(() => { toggleLaunchArea(false) }, 10000)
 
-                    setLaunchDetails('準備OK。参加勢集合！')
+                    setLaunchDetails('読み込み完了！さぁ遊ぼう！')
 
                     // Init Discord Hook
                     const distro = DistroManager.getDistribution()
